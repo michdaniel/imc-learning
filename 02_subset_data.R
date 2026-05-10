3eb6b88a7b0c4ee8c24acecbc042e38569f382e8
michdaniel <michdaniel@gmail.com>
2026-05-10 14:29:15 +0200

    subset SCE to protein markers and R/NR annotated cells

---
+++ 02_subset_data.R
@@ -0,0 +1,22 @@
+# 02_subset_data.R
+# Subset SCE to protein markers and annotated patients only
+
+library(SingleCellExperiment)
+library(imcdatasets)
+
+# Load dataset
+spe <- HochSchulz_2022_Melanoma(data_type = "sce")
+
+# Subset to protein markers only
+protein_markers <- rownames(spe)[rowData(spe)$marker_no_RNA == TRUE]
+spe_protein <- spe[protein_markers, ]
+
+# Subset to cells with response annotation (R or NR only)
+annotated_cells <- spe$patient_status_at_3months %in% c("R", "NR")
+spe_protein <- spe_protein[, annotated_cells]
+
+# Check dimensions
+dim(spe_protein)
+
+# Verify
+table(spe_protein$patient_status_at_3months)
